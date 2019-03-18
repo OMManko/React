31a80ba3ffdfab8e83dac4d3d7d7f98378c73182
Volha_Manko <Volha_Manko@epam.com>
2019-03-18 09:51:19 +0300

    Webpack - task implementation

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,15 @@ module.exports = (env, options) => {
                         "css-loader",
                         "sass-loader",
                     ],
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {},
+                        },
+                    ],
                 }
             ]
         },
