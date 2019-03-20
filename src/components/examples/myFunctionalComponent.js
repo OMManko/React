88a16cf83b2bec12df5e2e03c8ce5c20262f5d53
Volha_Manko <Volha_Manko@epam.com>
2019-03-20 15:42:51 +0300

    Code refactoring after review

--- src/components/examples/myFunctionalComponent.js
+++ src/components/examples/myFunctionalComponent.js
@@ -2,14 +2,12 @@ import React from "react";
 import { hot } from "react-hot-loader";
 import PropTypes from "prop-types";
 
-function MyFunctionalComponent (props) {
-    return (
-        <div className="component-block">
-            <p>Functional component example</p>
-            <h1>Hello, {props.name}</h1>
-        </div>
-    );
-}
+const MyFunctionalComponent = ({ name }) => (
+    <div className="component-block">
+        <p>Functional component example</p>
+        <h1>Hello, {name}</h1>
+    </div>
+);
 
 MyFunctionalComponent.propTypes = {
     name: PropTypes.string.isRequired
